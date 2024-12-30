5e29f2fbbf785f4377ddc77f2978fb1ec6d781d3
Jonathan <titanbar@hotmail.com>
2024-12-31 00:07:26 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,105 +1,114 @@
-input.onButtonPressed(Button.A, function () {
-    direction*=-1;
-})
-
-let line1: neopixel.Strip = null
-let green = 0
-let blue = 0
-let red = 0
-let direction = 1;
-red = 50
-blue = 50
-green = 50
-led.enable(false)
-let strip = neopixel.create(DigitalPin.P3, 32, NeoPixelMode.RGB)
-strip.setBrightness(30)
-line1 = strip.range(0, 8)
-strip.showColor(neopixel.colors(NeoPixelColors.Black))
-
-
-let position = 0;
-let previousPosition = 0;
-basic.forever(function () {
-    //moveLines();
-    runDetect();
-})
-
-function moveLines(){
-    moveOnLine(0)
-    moveOnLine(2)
-    changePosition();
-    basic.pause(20);
-}
-
-function changePosition(){
-    previousPosition = position;
-    let next = position + direction;
-    if(next < 0 || next >=8){
-        direction*=-1;
-        next = position + direction;
+class ButtonDectector {
+    onPressedFunction: (isA: boolean, isB: boolean) => void;
+    onReleasedFunction: () => void;
+    constructor(private pressed: boolean = false) { }
+    onPressed(fct: (isA: boolean, isB: boolean) => void): ButtonDectector {
+        this.onPressedFunction = fct;
+        return this;
     }
-    position = next
-}
-
-function moveOnLine(line: number){
-    strip.range(line*8 + previousPosition, 1).showColor(NeoPixelColors.Black);
-    strip.range(line * 8 + position, 1).showColor(NeoPixelColors.Green);    
-}
-
-/* Light projector part */
-
-let minBrightness = 40;
-let color = 0;
-input.onLogoEvent(TouchButtonEvent.Pressed,()=>minBrightness = (minBrightness + 20) % 255);
-
-const colors = [NeoPixelColors.Red,NeoPixelColors.Orange, NeoPixelColors.Yellow, NeoPixelColors.Green, NeoPixelColors.Blue, NeoPixelColors.Indigo, NeoPixelColors.Violet, NeoPixelColors.Purple, NeoPixelColors.White];
-
-function turnOn(isA: boolean, isB: boolean){
-    if(isA && isB){
-        // Change color
-        color = (color+1) % colors.length;
-    }else{
-        strip.setBrightness(isA ? 255 : minBrightness);
-        strip.showColor(colors[color]);
+    onReleased(fct: () => void): ButtonDectector {
+        this.onReleasedFunction = fct;
+        return this;
     }
-}
-
-function turnOff() {
-    strip.showColor(NeoPixelColors.Black);
-}
-
-const detector = getDetectPressed().onPressed(turnOn).onReleased(turnOff);
-
-function runDetect(){
-    detector.detect();
-}
-
-function getDetectPressed(){
-    class ButtonDectector{
-        onPressedFunction: (isA: boolean, isB: boolean) => void;
-        onReleasedFunction: () => void;
-        constructor(private pressed: boolean = false){}
-        onPressed(fct: (isA: boolean, isB: boolean) => void): ButtonDectector {
-            this.onPressedFunction = fct;
-            return this;
-        }
-        onReleased(fct: () => void): ButtonDectector {
-            this.onReleasedFunction = fct;
-            return this;
-        }
-        detect(){
-            const isAPressed = input.buttonIsPressed(Button.A);
-            const isBPressed = input.buttonIsPressed(Button.B);
-            if (!this.pressed && (isAPressed || isBPressed)) {
-                this.pressed = true
-                this.onPressedFunction(isAPressed, isBPressed)
-            } else {
-                if (this.pressed && !isAPressed && !isBPressed) {
-                    this.pressed = false
-                    this.onReleasedFunction()       
-                }
+    detect() {
+        const isAPressed = input.buttonIsPressed(Button.A);
+        const isBPressed = input.buttonIsPressed(Button.B);
+        if (!this.pressed && (isAPressed || isBPressed)) {
+            this.pressed = true
+            this.onPressedFunction(isAPressed, isBPressed)
+        } else {
+            if (this.pressed && !isAPressed && !isBPressed) {
+                this.pressed = false
+                this.onReleasedFunction()
             }
         }
     }
-    return new ButtonDectector();
 }
+
+class Projector{
+    constructor(private color:number=0, private minBrightness: number = 40){
+        input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+            this.minBrightness = (this.minBrightness + 20) % 255
+        })
+    }
+    turnOn(isA: boolean, isB: boolean){
+        if (isA && isB) {
+            // Change color
+            this.color = (this.color + 1) % colors.length
+        } else {
+            strip.setBrightness(isA ? 255 : this.minBrightness)
+            strip.showColor(colors[this.color])
+        }
+    }
+    turnOff(){
+        strip.showColor(NeoPixelColors.Black)
+    }
+}
+
+class RandomPoint{
+    run(){
+        const pos = randint(0, 31)
+        const color = randint(0, colors.length - 1)
+        strip.clear()
+        strip.show()
+        strip.range(pos, 1).showColor(colors[color])
+        basic.pause(40)
+    }
+}
+
+class MoveLines{
+    constructor(private position: number = 0, 
+    private previousPosition: number = 0, 
+    private direction: number=1){}
+    execute(){
+        this.moveOnLine(0)
+        this.moveOnLine(2)
+        this.changePosition()
+        basic.pause(20)
+    }
+    moveOnLine(line: number) {
+        strip.range(line * 8 + this.previousPosition, 1).showColor(NeoPixelColors.Black)
+        strip.range(line * 8 + this.position, 1).showColor(NeoPixelColors.Green)
+    }
+    changePosition() {
+        this.previousPosition = this.position;
+        let next = this.position + this.direction;
+        if (next < 0 || next >= 8) {
+            this.direction *= -1;
+            next = this.position + this.direction
+        }
+        this.position = next
+    }
+}
+
+let strip: neopixel.Strip = null
+let red = 50
+let blue = 50
+let green = 50
+led.enable(false)
+strip = neopixel.create(DigitalPin.P3, 32, NeoPixelMode.RGB)
+strip.setBrightness(30)
+let line1 = strip.range(0, 8)
+strip.showColor(neopixel.colors(NeoPixelColors.Black))
+// Light projector part
+const colors = [
+NeoPixelColors.Red,
+NeoPixelColors.Orange,
+NeoPixelColors.Yellow,
+NeoPixelColors.Green,
+NeoPixelColors.Blue,
+NeoPixelColors.Indigo,
+NeoPixelColors.Violet,
+NeoPixelColors.Purple,
+NeoPixelColors.White
+]
+const projector = new Projector();
+const detector = new ButtonDectector().onPressed((a,b)=>projector.turnOn(a,b)).onReleased(()=>projector.turnOff());
+const moves = new MoveLines();
+const randPoint = new RandomPoint();
+basic.forever(function () {
+    // moveLines();
+    //moves.execute();
+    // detector.detect();
+    randPoint.run();
+})
